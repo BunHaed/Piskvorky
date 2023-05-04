c78ae3e9ac2175f480678abccd39309f755b1dd8
BunHaed <marius234582@gmail.com>
2023-05-04 19:34:47 +0200

    test

--- Piškvorky/Program.cs
+++ Piškvorky/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
 
- //test :)
- //tesr
- //asd
+Console.WriteLine("test");
